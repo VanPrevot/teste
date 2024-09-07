456d22da75f44fdcc217323f68f8d9fe909d244a
Vanessa Prevot <profprevot@gmail.com>
2024-09-07 17:44:03 -0300

    Meu primeiro Commit

---
+++ App10/App10/App10/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+
+public class MainPageViewModel : BindableObject
+    {
+        public ICommand Item1Command { get; }
+        public ICommand Item2Command { get; }
+        public ICommand Item3Command { get; }
+        public ICommand Item4Command { get; }
+
+            public MainPageViewModel()
+        {
+            // Inicializa os comandos com navegação para novas páginas
+            Item1Command = new Command(OnItem1Clicked);
+            Item2Command = new Command(OnItem2Clicked);
+            Item3Command = new Command(OnItem3Clicked);
+            Item4Command = new Command(OnItem4Clicked);
+            }
+
+        private async void OnItem1Clicked()
+        {
+                await Application.Current.MainPage.Navigation.PushAsync(new Page1());
+            }
+
+        private async void OnItem2Clicked()
+        {
+                await Application.Current.MainPage.Navigation.PushAsync(new Page2());
+            }
+
+        private async void OnItem3Clicked()
+        {
+                await Application.Current.MainPage.Navigation.PushAsync(new Page3());
+            }
+        private async void OnItem4Clicked()
+         {
+               await Application.Current.MainPage.Navigation.PushAsync(new Page4());
+           }
+        }
+
+}
+}
---
+++ App10/App10/App10/Page4.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page4 : ContentPage
+    {
+        public Page4()
+        {
+            InitializeComponent();
+        }
+
+        private async void btEnviar_Clicked(object sender, EventArgs e)
+        {
+            String re = etRemetente.Text;
+            String des = etDestino.Text;
+            String ass = etAssunto.Text;
+            String texto = etTexto.Text;
+
+            String url = "mailto:" + des + "?subject=" + ass + "&body=" + texto;
+            Device.OpenUri(new Uri(url));
+
+            // Aguarda 2 segundos antes de voltar para a MainPage
+            await Task.Delay(2000);
+
+            // Retorna à MainPage
+            await Navigation.PopToRootAsync();
+        }
+
+        private void btLimpar_Clicked(object sender, EventArgs e)
+        {
+            // Limpa os campos de entrada de dados
+            etRemetente.Text = string.Empty;
+            etDestino.Text = string.Empty;
+            etAssunto.Text = string.Empty;
+            etTexto.Text = string.Empty;
+        }
+    }
+}
